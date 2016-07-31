b1795915ae77b9270df2a80cea1f1e7f3e2bd1ae
Jaxxa <jaxxa90@gmail.com>
2016-07-31 17:59:27 +1000

    Initial Setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ED-TurretControl
+# ED-Mod
+This is the Readme for the Enhanced Devemopment Mod, ED-TurretControl, for the Game Rimworld.
+
+Aditional details can be fount in About.xml
+
+#Change Log
+
+01.00.00
+*Alpha 14 Update
---
+++ Source/ED-TurretControl/ED-TurretControl/Detours/_Building_TurretGun.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace ED_TurretControl.Detours
+{
+    internal class _Building_TurretGunt : RimWorld.Building_TurretGun
+    {
+
+        private Boolean _CanSetForcedTarget
+        {
+            get
+            {
+                //This mirrors the A14 Implementation of CanToggleHoldFire
+
+                //Allow for all Turrets belonging to the Player
+                if (this.Faction == Faction.OfPlayer)
+                {
+                    return true;
+                }
+
+                //Allow for all Turrets manned by the player.
+                if (this.mannableComp != null && this.mannableComp.ManningPawn != null)
+                {
+                    return this.mannableComp.ManningPawn.Faction == Faction.OfPlayer;
+                }
+
+                //Return False if neither  of those conditions has been met.
+                return false;
+
+
+            }
+        }
+
+    }
+}
